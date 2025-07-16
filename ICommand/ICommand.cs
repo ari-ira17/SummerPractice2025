d6db0ea14b08893a98111b7dd8e6651dc2b4d326
ari-ira17 <ari.ira17@yandex.ru>
2025-07-16 13:38:22 +0600

    feat: add test command implementation

--- ICommand/ICommand.cs
+++ ICommand/ICommand.cs
@@ -3,4 +3,20 @@
 public interface ICommand
 {
     void Execute();
+    bool IsCompleted();
+}
+
+public class TestCommand(int id) : ICommand
+{
+    int counter = 0;
+
+    public void Execute()
+    {
+        Console.WriteLine($"Поток {id} вызов {++counter}");
+    }
+
+    public bool IsCompleted()
+    {
+        return counter >= 2;
+    }
 }
