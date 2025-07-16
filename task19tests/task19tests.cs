6d00f6f2e6c1dcb2128edd85efa2deb13d3be4b5
ari-ira17 <ari.ira17@yandex.ru>
2025-07-16 13:39:21 +0600

    feat: add test

--- task19tests/task19tests.cs
+++ task19tests/task19tests.cs
@@ -1,10 +1,48 @@
-namespace task19tests;
+using Command;
+using System.Threading;
 
-public class UnitTest1
+public class ServerThreadTests
 {
     [Fact]
-    public void Test1()
+    public void Main_Test()
     {
+        var server_thread = new ServerThread();
+        var test_commands = new TestCommand[]
+        {
+            new TestCommand(1),
+            new TestCommand(2),
+            new TestCommand(3),
+            new TestCommand(4),
+            new TestCommand(5)
+        };
 
+        server_thread.Start();
+
+        for (int i = 0; i < test_commands.Length; i++)
+        {
+            server_thread.AddToQueue(test_commands[i]);
+        }
+
+        while (server_thread.is_running)
+        {
+            bool check = true;
+            foreach (TestCommand test_command in test_commands)
+            {
+                check = check && test_command.IsCompleted();
+            }
+
+            if (check)
+            {
+                server_thread.AddToQueue(new HardStopCommand(server_thread));
+                break;
+            }
+
+            Thread.Sleep(100);
+        }
+
+        Thread.Sleep(500);
+
+        Assert.True(test_commands.All(command => command.IsCompleted()));
+        Assert.False(server_thread.is_running);
     }
 }
